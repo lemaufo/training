a299c786c287693da751f91d03a785dc35fe90f5
fr640504-dotcom <fr640504@gmail.com>
2026-05-12 13:51:30 -0600

    Vista de Fatima

---
+++ app/Http/Controllers/FatimaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FatimaController extends Controller
+{
+        public function index()
+    {
+        return view('fatima');
+    }
+}
