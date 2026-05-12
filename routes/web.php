d4f2c3fdcf407e2c7254b199b424f74803148d27
hannia lysset gutierrez lopez <gutierrezlopezhannia@gmail.com>
2026-05-12 13:30:48 -0600

    Vista de Hannia

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
+Route::get('/hannia', function () {
+    return view('hannia');
+});
+
 require __DIR__.'/auth.php';
